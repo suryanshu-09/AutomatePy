4fd8f7c0b73f18462148743f729b12f5e481c09e
suba <suryanshusaini@gmail.com>
2023-06-05 12:10:18 +0530

    Added Files

---
+++ README.md
@@ -0,0 +1,2 @@
+#Automate.Py
+This repo contains all my notes from the ISTE workshop on 14th May, 23 called Automate.Py
---
+++ test.py
@@ -0,0 +1,13 @@
+import cowsay
+import sys
+"""
+def main():
+    try: 
+        cowsay.tux(sys.argv[1])
+    except IndexError:
+        print("hello")
+main()
+"""
+import pandas as pd
+df = pd.read_csv("~/Documents/Workshops/Automate/Salary_2.csv")
+print(df)
